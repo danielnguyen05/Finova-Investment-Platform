a025d240e163afde222ac1c79216af2272de9d5c
Zenan <chenzn50@gmail.com>
2025-01-21 14:48:11 +1100

    Changed directory name; implemented get_real_gdp

---
+++ Economic_Indicators/data_ei.py
@@ -0,0 +1,32 @@
+import requests, json
+
+API_KEY_2 = "DEMO" # 6Z27NWGRHMUYEX31 for when we wanna do it fr
+SUCCESS = 200
+GDP_FNAME = "Real_GDP.json"
+
+def get_real_gdp(interval: str) -> json:
+    '''
+    Grabs the real GDP from the API.
+
+    Input:
+    Interval: quarterly/annual.
+
+    Output:
+    json file containing all relevant information.
+    '''
+    url = f'https://www.alphavantage.co/query?function=REAL_GDP&interval={interval}&apikey={API_KEY_2}'
+    response = requests.get(url)
+
+    if response.status_code == SUCCESS:
+        gdp_data = response.json()
+
+        with open(GDP_FNAME, "w") as json_file:
+            json.dump(gdp_data, json_file, indent=4)  
+            print(f"JSON data saved to {GDP_FNAME}")
+
+        return gdp_data 
+    
+    else:
+        print(f"Error: {response.status_code} - {response.reason}")
+        return None
+
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from Corporate_Information.data_ci import get_company_overview
+from Economic_Indicators.data_ei import *
 
 def main():
     symbol = "AAPL"  
     data = get_company_overview(symbol)
-    
+
     if data:
         print(f"Company Name: {data.get('Name')}")
         print(f"Sector: {data.get('Sector')}")
