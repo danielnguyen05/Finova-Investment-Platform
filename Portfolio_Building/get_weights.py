2f3a45a7514cdfa3855bca7cfc2994930e72f3c2
Zenan <chenzn50@gmail.com>
2025-02-03 16:02:27 +1100

    Changed from dual form to primal form of LP

--- Portfolio_Building/get_weights.py
+++ Portfolio_Building/get_weights.py
@@ -1,6 +1,6 @@
 import sys, os
+from pulp import LpProblem, LpVariable, lpSum, LpMaximize, value, PULP_CBC_CMD
 from random import randint
-from pulp import LpProblem, LpVariable, lpSum, LpMinimize, value, PULP_CBC_CMD
 
 parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(parent_dir)
@@ -9,49 +9,51 @@ from Portfolio_Building.get_variables import get_variables
 
 COMPANY_COUNT = 5
 
-def _get_weights(threshold: float, expected_ror: list[float], beta: list[float]) -> list[float]:
+def _get_weights(threshold: float, expected_ror: list[float], beta: list[float]) -> tuple[list[float], float]:
     '''
-    Gets the weights associated with each company, listed in the file get_variables.py.
+    Gets the optimal weights for each company to maximize expected return while meeting a beta constraint.
 
     Input:
-    threshold: the required rate of return for the portfolio that has to be met
-    expected_ror: list of expected rates of returns for a few given companies
-    beta: list of beta for a few given companies
+    - threshold: The required beta for the portfolio.
+    - expected_ror: List of expected rates of returns for companies.
+    - beta: List of beta values for the companies.
 
     Output:
-    List of floats detailing what weights should be assigned to each company, as a percentage.
+    - Tuple (list of weights, maximum expected return)
     '''
-    problem = LpProblem("Minimize_Beta_Given_ExRoR", LpMinimize)
+    problem = LpProblem("Maximise_Expected_RoR_Given_Beta", LpMaximize)
     weights = [LpVariable(f"w_{i+1}", lowBound=randint(1, 5)/100, upBound=randint(37, 43)/100) for i in range(COMPANY_COUNT)]
     problem += lpSum(weights) == 1
-    problem += lpSum(weights[i] * expected_ror[i] for i in range(COMPANY_COUNT)) == threshold
-    problem += lpSum(weights[i] * beta[i] for i in range(5))
-    problem.solve(PULP_CBC_CMD(msg=False))
-    weights = [round(value(i) * 100, 2) for i in weights]
-    return weights
+    problem += lpSum(weights[i] * beta[i] for i in range(COMPANY_COUNT)) <= threshold
+    problem += lpSum(weights[i] * expected_ror[i] for i in range(COMPANY_COUNT))
+    problem.solve(PULP_CBC_CMD(msg=False))  
+    weights = [round(value(w) * 100, 2) for w in weights]
+    return weights, value(problem.objective)
 
 def get_weights_given_aggressiveness(aggressiveness: str) -> tuple[list[float], float]:
     '''
-    Returns the appropriate weights for the given companies for a certain level of aggressiveness.
+    Returns optimal portfolio weights based on the chosen level of risk aggressiveness.
 
     Input:
-    aggressiveness: conservative, moderately conservative, moderately aggressive, aggressive.
+    - aggressiveness: "conservative", "moderately conservative", "moderately aggressive", "aggressive".
 
     Output:
-    Tuple of list of floats detailing what weights should be assigned to each company, as well as a float
-    which says what the target expected rate of return is.
+    - Tuple (list of weights, target expected rate of return).
     '''
-    to_be_processed = sorted(get_variables())
+    to_be_processed = get_variables()
     expected_ror, beta = [elem[0] for elem in to_be_processed], [elem[1] for elem in to_be_processed]
+    sorted_beta = sorted(beta)
+
     if aggressiveness == "conservative":
-        target_ror = (expected_ror[0] + expected_ror[1]) / 2
-        return (_get_weights(target_ror, expected_ror, beta), target_ror)
+        target_beta = (sorted_beta[0] + sorted_beta[1]) / 2 + 0.15 # Stops LP from cycling
     elif aggressiveness == "moderately conservative":
-        target_ror = (expected_ror[1] + expected_ror[2]) / 2
-        return (_get_weights(target_ror, expected_ror, beta), target_ror)
+        target_beta = (sorted_beta[1] + sorted_beta[2]) / 2
     elif aggressiveness == "moderately aggressive":
-        target_ror = (expected_ror[2] + expected_ror[3]) / 2
-        return (_get_weights(target_ror, expected_ror, beta), target_ror)
+        target_beta = (sorted_beta[2] + sorted_beta[3]) / 2
     else:
-        target_ror = (expected_ror[3] + expected_ror[4]) / 2 - 0.01
-        return (_get_weights(target_ror, expected_ror, beta), target_ror)
+        target_beta = (sorted_beta[3] + sorted_beta[4]) / 2
+
+    weights = ([], 0)
+    while not weights[0] or any(list(map(lambda x: x <= 0, weights[0]))):
+        weights = _get_weights(target_beta, expected_ror, beta)
+    return weights
